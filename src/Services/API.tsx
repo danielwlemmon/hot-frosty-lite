a6466920e4f98301046b01c835535848e53289cd
Daniel Lemmon <95183188+danielwlemmon@users.noreply.github.com>
2022-07-17 10:47:29 -0600

    update API URL

--- src/Services/API.tsx
+++ src/Services/API.tsx
@@ -8,7 +8,7 @@ const API = {
     + '&exclude=hourly,minutely,alerts&units=imperial&appid=58d0361f438e67f6c23c40e7d62a5c84')
   )},
   getLatLong: (zip:string) => {
-    return axios.get('http://api.positionstack.com/v1/forward?access_key=3f10f040b0272fd0c5f95ea64844da9e&query='
+    return axios.get('https://api.positionstack.com/v1/forward?access_key=3f10f040b0272fd0c5f95ea64844da9e&query='
     + zip );
   },
 };
